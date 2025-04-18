558fb8c87b2f3fa36783169d41144f1018ec18c4
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-18 21:07:43 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lambda180425
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
