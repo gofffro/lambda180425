ff356f4f503d417f52b6f5538b33862f94d76d9a
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-18 21:46:24 +0700

    Добавлен класс BinaryTree и вложенный класс Node для представления узла дерева

---
+++ BinaryTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda180425
+{
+  public class BinaryTree<T>
+  {
+    private Node root;
+
+    private class Node
+    {
+      public T Value { get; set; }
+      public Node Left { get; set; }
+      public Node Right { get; set; }
+      public Node Parent { get; set; }
+    }
+  }
+}
