a7d50ffc61994e92b90afc65416f29cfc7702864
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-18 23:17:14 +0700

    Создание базовой структуры класса BinaryTree

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lambda180425
+﻿namespace lambda180425
 {
   public class BinaryTree<T>
   {
-    private Node root;
+    private readonly Func<T, T, int> _comparer;
 
-    private class Node
+    public BinaryTree(Func<T, T, int> comparer)
     {
-      public T Value { get; set; }
-      public Node Left { get; set; }
-      public Node Right { get; set; }
-      public Node Parent { get; set; }
+      if (comparer == null)
+      {
+        throw new ArgumentNullException(nameof(comparer));
+      }
+      _comparer = comparer;
     }
   }
 }
