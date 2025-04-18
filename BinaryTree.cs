7e5236c7d28c2281c226bcba2c180219b6fe69c7
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-18 23:26:47 +0700

    Добавлен внутренний класс InOrderEnumerator, реализующий интерфейс IEnumerator<T> для обхода дерева.  Метод MoveNext обеспечивает переход к следующему узлу, а метод Reset сбрасывает итератор в начальное состояние. Реализованы свойства Current и метод Dispose

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -63,5 +63,57 @@
     public IEnumerator<T> GetEnumerator() => new InOrderEnumerator(_root);
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+    private class InOrderEnumerator : IEnumerator<T>
+    {
+      private Node<T> _current;
+      private readonly Node<T> _root;
+      private readonly Stack<Node<T>> _stack;
+
+      public InOrderEnumerator(Node<T> root)
+      {
+        _root = root;
+        _stack = new Stack<Node<T>>();
+        InitializeStack(root);
+      }
+
+      private void InitializeStack(Node<T> node)
+      {
+        while (node != null)
+        {
+          _stack.Push(node);
+          node = node.Left;
+        }
+      }
+
+      public T Current => _current.Data;
+      object IEnumerator.Current => Current;
+
+      public bool MoveNext()
+      {
+        if (_stack.Count == 0)
+        {
+          return false;
+        }
+
+        _current = _stack.Pop();
+        var node = _current.Right;
+        while (node != null)
+        {
+          _stack.Push(node);
+          node = node.Left;
+        }
+        return true;
+      }
+
+      public void Reset()
+      {
+        _stack.Clear();
+        InitializeStack(_root);
+        _current = null;
+      }
+
+      public void Dispose() { }
+    }
+
   }
 }
